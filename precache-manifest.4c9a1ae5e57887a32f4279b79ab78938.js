910ba8ab7bebdd5f4c212c525e60b4c28b138a0b
System Administrator <root@Solanges-MacBook-Air.local>
2020-03-25 14:34:56 -0400

    Updates

---
+++ precache-manifest.4c9a1ae5e57887a32f4279b79ab78938.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cab9774276d13187c44acd301eb5c9",
+    "url": "/dogapi/index.html"
+  },
+  {
+    "revision": "4c24f1f3be3e84fe7f37",
+    "url": "/dogapi/static/css/main.70e7e0a6.chunk.css"
+  },
+  {
+    "revision": "ccc2ed81db1d7a784c94",
+    "url": "/dogapi/static/js/2.90fc6046.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dogapi/static/js/2.90fc6046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c24f1f3be3e84fe7f37",
+    "url": "/dogapi/static/js/main.9b8769f4.chunk.js"
+  },
+  {
+    "revision": "d7b6c74c1b9113d4f692",
+    "url": "/dogapi/static/js/runtime-main.0bda3c86.js"
+  },
+  {
+    "revision": "bb5574e809b292e246a5f0176e9a1e0d",
+    "url": "/dogapi/static/media/dog.bb5574e8.png"
+  }
+]);
